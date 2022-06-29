fe07ea8af69f5a91b32585d28ad68af01447c163
Sreedev10 <sreedevskurup10@gmail.com>
2022-06-29 09:41:33 -0700

    completed

---
+++ form.js
@@ -0,0 +1,51 @@
+
+registerBtnEvent=()=>{
+     
+    employeename=(document.getElementById("firstname").value)
+    address=(document.getElementById("secondname").value)
+    age=(document.getElementById("age").value)
+    gender=(document.getElementById("gender").value)
+    emailid=(document.getElementById("email id").value)
+    password=(document.getElementById("password").value)
+    confirmpassword=(document.getElementById("confirm password").value)
+    address=(document.getElementById("address").value)
+    city=(document.getElementById("city").value)
+    state=(document.getElementById("state").value)
+    mobileno=(document.getElementById("mobileno.").value)   
+
+    console.log(firstname)
+    alert(firstname)
+
+    console.log(secondname)
+    alert(secondname)
+    
+    console.log(age)
+    alert(age)
+
+    console.log(gender)
+    alert(gender)
+
+    console.log(emailid)
+    alert(emailid)
+
+    console.log(password)
+    alert(password)
+
+    console.log(confirmpassword)
+    alert(confirmpassword)
+
+    console.log(address)
+    alert(address)
+
+    console.log(city)
+    alert(city)
+
+    console.log(state)
+    alert(state)
+
+    console.log(mobileno)
+    alert(mobileno)
+   
+}
+
+
